cccce620229b274cab59a20e275862f60f2ae5d9
KangIn Park <pkihome@naver.com>
2020-06-12 11:48:11 +0900

    기능: keep_alive 구현

--- Computer Networks/A4/Server.py
+++ Computer Networks/A4/Server.py
@@ -1,15 +1,42 @@
 import sys
+import time
+import threading
 from socket import *
+
+def alive_chk():
+    global alive
+    global clist
+    while True:
+        tmp = list(alive.items())
+        for key, val in tmp:
+            if val + 30 < time.time():
+                print("{} is off-line".format(key.split(" ")[0]))
+                sys.stdout.flush()
+                del alive[key]
+                clist.remove(key)
+                send = "1" + ",".join(clist)
+                for data in clist:
+                    ip = data.split(" ")[1]
+                    port = int(data.split(" ")[2])
+                    ServerSocket.sendto(send.encode(),(ip, port))
+                break
+        time.sleep(0.1)
+
 
 ServerSocket = socket(AF_INET, SOCK_DGRAM)
 ServerSocket.bind(('', 10080))
 clist = []
+alive = {}
+th = threading.Thread(target=alive_chk)
+th.daemon = True
+th.start()
 while True:
     msg, Add = ServerSocket.recvfrom(65565)
     op = msg.decode()[0]  # 0:registration 1:list data 2:chat data 3:keep_alive data 4:unregistration
     if op == "0":
         clist.append(msg.decode()[1:] +" " + Add[0] + " " + str(Add[1]))
-        print("{}\t{}:{}\n".format(msg.decode()[1:], Add[0], Add[1]))
+        alive[clist[-1]] = time.time()
+        print("{}\t{}:{}".format(msg.decode()[1:], Add[0], Add[1]))
         sys.stdout.flush()
         send = "1" + ",".join(clist)
         for data in clist:
@@ -17,13 +44,14 @@ while True:
             port = int(data.split(" ")[2])
             ServerSocket.sendto(send.encode(),(ip, port))
     elif op == "3":
-        pass
+        alive[msg.decode()[1:] + " " + Add[0] + " " + str(Add[1])] = time.time()
     elif op == "4":
         for i in range(0, len(clist)):
             tmp = clist[i].split(" ")
             if tmp[1] == Add[0] and tmp[2] == str(Add[1]):
+                del alive[clist[i]]
                 del clist[i]
-                print("{} is unregistered\t{}:{}\n".format(tmp[0], tmp[1], tmp[2]))
+                print("{} is unregistered\t{}:{}".format(tmp[0], tmp[1], tmp[2]))
                 sys.stdout.flush()
                 send = "1" + ",".join(clist)
                 for data in clist:
